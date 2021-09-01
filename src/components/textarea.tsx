c48fa7661e4d1d919a46e863ffd4facf2c8e1a8c
zerowong <wongzero@foxmail.com>
2021-09-02 00:54:39 +0800

    feat: add `textarea` component

---
+++ src/components/textarea.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import { Transition } from '@headlessui/react'
+import { Icon } from './icon'
+
+type TextAreaProps = JSX.IntrinsicElements['textarea'] & {
+  /**
+   * 容器类名
+   */
+  wrapperClassName?: string
+  /**
+   * 容器样式
+   */
+  wrapperStyle?: React.CSSProperties
+  /**
+   * 是否有边框，默认`true`
+   */
+  bordered?: boolean
+  /**
+   * 可以点击清除图标删除内容
+   */
+  clearable?: boolean
+  /**
+   * 内联模式
+   */
+  inline?: boolean
+}
+
+export function TextArea(props: TextAreaProps) {
+  const {
+    bordered = true,
+    clearable = false,
+    value,
+    onChange,
+    defaultValue,
+    inline,
+    wrapperClassName,
+    wrapperStyle,
+    className,
+    disabled,
+    onFocus,
+    onBlur,
+    ...rest
+  } = props
+
+  const [innerValue, setInnerValue] = useState(() => {
+    if (typeof defaultValue === 'string') {
+      return defaultValue
+    }
+    if (typeof defaultValue === 'number') {
+      return defaultValue.toString()
+    }
+    if (defaultValue instanceof Array) {
+      return defaultValue.join(',')
+    }
+    return ''
+  })
+  const handleValueChange: React.ChangeEventHandler<HTMLTextAreaElement> = (
+    e
+  ) => {
+    setInnerValue(e.target.value)
+  }
+
+  const [wrapperHover, setWrapperHover] = useState(false)
+  const handleWrapperMouseEnter = () => {
+    setWrapperHover(true)
+  }
+  const handleWrapperMouseLeave = () => {
+    setWrapperHover(false)
+  }
+
+  const [inputIsFocus, setInputIsFocus] = useState(false)
+  const handleInputFocus: React.FocusEventHandler<HTMLTextAreaElement> = (
+    e
+  ) => {
+    setInputIsFocus(true)
+    onFocus?.(e)
+  }
+  const handleInputBlur: React.FocusEventHandler<HTMLTextAreaElement> = (e) => {
+    setInputIsFocus(false)
+    onBlur?.(e)
+  }
+
+  return (
+    <div
+      className={classNames(
+        'transition duration-300 inline-flex items-center px-3',
+        {
+          'border rounded': bordered,
+          'w-full': !inline,
+          'w-75': inline,
+          'cursor-text': !disabled,
+          'bg-gray-300': disabled,
+          'hover:border-sky-500': bordered && !disabled,
+          'border-sky-500': bordered && inputIsFocus,
+        },
+        wrapperClassName
+      )}
+      style={wrapperStyle}
+      onMouseEnter={handleWrapperMouseEnter}
+      onMouseLeave={handleWrapperMouseLeave}
+    >
+      <textarea
+        className={classNames(
+          'outline-none py-2 flex-grow disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-400 transition duration-300',
+          className
+        )}
+        value={value ?? innerValue}
+        onChange={onChange ?? handleValueChange}
+        disabled={disabled}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+      />
+      <Transition
+        as="span"
+        show={clearable && innerValue.length > 0 && wrapperHover && !disabled}
+        enter="transition ease-out duration-300"
+        enterFrom="transform-gpu opacity-0 scale-0"
+        enterTo="transform-gpu opacity-100 scale-100"
+        leave="transition ease-in duration-200"
+        leaveFrom="transform-gpu opacity-100 scale-100"
+        leaveTo="transform-gpu opacity-0 scale-0"
+        className="ml-2"
+      >
+        <Icon
+          name="close-fill"
+          className="text-gray-400 cursor-pointer hover:text-gray-500"
+          onClick={() => {
+            setInnerValue('')
+          }}
+        />
+      </Transition>
+    </div>
+  )
+}
